2da9b4b701bffbdf93bbe416c15d0cc74623f24f
Leenuus <linusrach24@gmail.com>
2024-03-03 21:51:20 +0800

    macro works now

--- lua/recording.lua
+++ lua/recording.lua
@@ -1,30 +1,33 @@
 -- Popup message when recording
--- FIXME: marco doesn't work!!!
 
--- vim.api.nvim_create_augroup("recording", { clear = true })
---
--- local noption = {
---   title = "Recording",
---   timeout = 1,
---   hide_from_history = true,
---   icon = ''
--- }
--- local level = vim.log.levels.INFO
---
--- vim.api.nvim_create_autocmd("RecordingEnter", {
---   pattern = "*",
---   callback = function()
--- -- TODO: a maybe needed error handling for `require`
---     require("notify")("Start Recording", level, noption)
---   end,
---   group = "recording",
--- })
---
--- vim.api.nvim_create_autocmd("RecordingLeave", {
---   pattern = "*",
---   callback = function()
--- -- TODO: a maybe needed error handling for `require`
---     require("notify")("Stop Recording", level, noption)
---   end,
---   group = "recording",
--- })
+local nmap = require('helpers').map_normal
+
+nmap('q', 'q')
+
+vim.api.nvim_create_augroup("recording", { clear = true })
+
+local noption = {
+  title = "Recording",
+  timeout = 1,
+  hide_from_history = true,
+  icon = ''
+}
+local level = vim.log.levels.INFO
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  pattern = "*",
+  callback = function()
+-- TODO: a maybe needed error handling for `require`
+    require("notify")("Start Recording", level, noption)
+  end,
+  group = "recording",
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  pattern = "*",
+  callback = function()
+-- TODO: a maybe needed error handling for `require`
+    require("notify")("Stop Recording", level, noption)
+  end,
+  group = "recording",
+})
