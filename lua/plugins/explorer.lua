35c9e349224cccd849bf54310be98bf2f4257a41
Leenuus <linusrach24@gmail.com>
2024-03-05 21:14:25 +0800

    Fuckup NvimTree, I am going to use my favorite `nnn`
    1. Use nnn as nvim file picker
    2. Add custom keybindings
    3. Add some futher tweaking todo notes, maybe as vimscript learning practice

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,22 +1,25 @@
--- TODO: a better alternative to create files and navigate it
--- I can build it my own
--- especially for the `file creator part`
--- I want one open a floating window prompt me for file name
+local tmap = require("helpers").map_toggle
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    require("nvim-tree").setup({
-      on_attach = function(bufnr)
-        local api = require("nvim-tree.api")
-        api.config.mappings.default_on_attach(bufnr)
-        -- vim.keymap.set('n', '<Enter>', function()
-        --   api.node.open.edit()
-        --   api.tree.close()
-        -- end)
-        -- vim.keymap.set('n', 'l', api.node.open.edit, { buffer = true })
-        -- vim.keymap.set('n', 'h', api.node.navigate.parent_close, { buffer = true })
-      end,
-      filters = { custom = { "^.git$" } },
-    })
-  end,
+  {
+    "mcchrish/nnn.vim",
+    config = function()
+      require("nnn").setup({
+        -- NOTE: -o used to open file only on pressing <Enter>
+        -- -c used to open 8-color
+        -- experience is much more better now with nnn integration
+        -- TODO: going to use nnn as explorer when we open directory with
+        -- neovim
+        command = "nnn -C",
+        set_default_mappings = 0,
+        replace_netrw = 1,
+        action = {
+          -- ["<c-t>"] = "tab split",
+          ["<c-s>"] = "split",
+          ["<c-t>"] = "vsplit",
+          -- ["<c-o>"] = copy_to_clipboard,
+        },
+      })
+      tmap("e", "<cmd>NnnPicker %:p:h<CR>", "[E]xplorer")
+    end,
+  },
 }
