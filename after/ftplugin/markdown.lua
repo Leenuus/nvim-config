e6bf841132e7d2ff0c1ae54208c7a198021acd88
Leenuus <linusrach24@gmail.com>
2024-03-08 20:50:18 +0800

    add functions to remove trailing spaces

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -8,14 +8,18 @@ local function capitalize(str)
   return string.gsub(str, "(%a)([%w_']*)", titleCase)
 end
 
+local function strip_trailing_whitespace(str)
+  return string.gsub(str, "%s$", "")
+end
+
 nmap("Q", function()
-  -- NOTE: capitalize headings
   vim.cmd([[Format]])
   local buf = vim.api.nvim_buf_get_lines(0, 0, -1, true)
   for i, line in pairs(buf) do
     if string.match(line, "^#+") ~= nil then
       buf[i] = capitalize(line)
     end
+    buf[i] = strip_trailing_whitespace(line)
   end
   vim.api.nvim_buf_set_lines(0, 0, -1, true, buf)
 end)
