296c937ab5fc35e4abd757ddeb9cbc7b1c22ae23
Leenuus <linusrach24@gmail.com>
2024-03-09 13:45:17 +0800

    Update noice config to get rid of long and annoying messages
    1. skip messages on `written`, more or fewer lines
    2. route long messages to mini view at right bottom

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,27 +1,52 @@
--- TODO: shorten timeout
--- TODO: shorter popup; adjust popup to screen size
--- TODO: display popup at bottom right
+local function route_skip(find)
+  return {
+    filter = {
+      event = "msg_show",
+      kind = "",
+      find = find,
+    },
+    opts = { skip = true },
+  }
+end
+
+local function route_mini(filter)
+  return {
+    filter = filter,
+    view = "mini",
+  }
+end
+
+local routes = {
+  route_skip("written"),
+  route_skip("yanked"),
+  route_skip("more lines"),
+  route_skip("more line"),
+  route_skip("fewer lines"),
+  route_skip("fewer line"),
+  route_mini({
+    min_width = vim.api.nvim_win_get_width(0) / 4,
+  }),
+  route_mini({
+    min_height = vim.api.nvim_win_get_height(0) / 4,
+  }),
+}
+
 return {
   "folke/noice.nvim",
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
   config = function()
     require("noice").setup({
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          ["cmp.entry.get_documentation"] = true,
         },
       },
-      -- you can enable a preset for easier configuration
+      -- TODO: figure out what presets have done for us
       presets = {
         bottom_search = false, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
@@ -29,6 +54,7 @@ return {
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      routes = routes,
     })
   end,
 }
