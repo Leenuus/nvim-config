b7cfa006eb01d5fcddef4a894159b4b0718e70c0
Leenuus <linusrach24@gmail.com>
2024-03-10 17:05:45 +0800

    append a space to search pattern for accuracy

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -62,7 +62,7 @@ local opts = {
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    pattern = [[\b(KEYWORDS): ]], -- ripgrep regex
     -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   },
 }
