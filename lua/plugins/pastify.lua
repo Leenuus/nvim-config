181852123fcf1ea7741efa77e3b407997b025bce
Leenuus <linusrach24@gmail.com>
2024-03-10 17:07:06 +0800

    leave `pastify` silent fail when python env is not right

--- lua/plugins/pastify.lua
+++ lua/plugins/pastify.lua
@@ -2,15 +2,7 @@ return {
   "TobinPalmer/pastify.nvim",
   cmd = { "Pastify" },
   config = function()
-    -- require('pastify').setup {
-    --   opts = {
-    --     apikey = "YOUR API KEY (https://api.imgbb.com/)", -- Needed if you want to save online.
-    --   },
-    -- }
-    -- p
-    -- FIXME: it breaks when you are in non-global python venv
-    -- possible solutions: 1. lazy loading when needed
-    require("pastify").setup({
+    pcall(require("pastify").setup, {
       opts = {
         absolute_path = false, -- use absolute or relative path to the working directory
         -- apikey = "", -- Api key, required for online saving
@@ -21,7 +13,8 @@ return {
         html = '<img src="$IMG$" alt="">',
         markdown = "![]($IMG$)",
         tex = [[\includegraphics[width=\linewidth]{$IMG$}]],
-      },
-    })
+      }
+    }
+    )
   end,
 }
