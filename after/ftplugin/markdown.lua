004c8887721c7d94485dda465a85a4d340edfc9b
Leenuus <linusrach24@gmail.com>
2024-03-11 22:49:24 +0800

    open `spell` completions only in markdown buf;
    Also, disable custom markdown mapping...

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,21 +1,21 @@
-local nmap = require("helpers").map_normal
-
-local function titleCase(first, rest)
-  return first:upper() .. rest:lower()
-end
-
-local function capitalize(str)
-  return string.gsub(str, "(%a)([%w_']*)", titleCase)
-end
-
-nmap("Q", function()
-  vim.cmd([[Format]])
-  local buf = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-  for i, line in pairs(buf) do
-    if string.match(line, "^#+") ~= nil then
-      line = capitalize(line)
-    end
-    buf[i] = vim.fn.trim(line, " ", 2)
-  end
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, buf)
-end)
+-- local nmap = require("helpers").map_normal
+--
+-- local function titleCase(first, rest)
+--   return first:upper() .. rest:lower()
+-- end
+--
+-- local function capitalize(str)
+--   return string.gsub(str, "(%a)([%w_']*)", titleCase)
+-- end
+--
+-- nmap("Q", function()
+--   vim.lsp.buf.format()
+--   local buf = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+--   for i, line in pairs(buf) do
+--     if string.match(line, "^#+") ~= nil then
+--       line = capitalize(line)
+--     end
+--     buf[i] = vim.fn.trim(line, " ", 2)
+--   end
+--   vim.api.nvim_buf_set_lines(0, 0, -1, false, buf)
+-- end)
