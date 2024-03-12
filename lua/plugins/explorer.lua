4ebb1359da4dced0dad4775f55c41ec38c8b2cd5
Leenuus <linusrach24@gmail.com>
2024-03-12 15:10:25 +0800

    use lir as file explorer
    1. nnn sometimes emits bugs in neovim environment
    2. nnn plugins is not needed in neovim usecase, so nnn gives nothing better
    3. lir is easy to configure, small and nice, well-documented

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,36 +1,109 @@
 local tmap = require("helpers").map_toggle
 return {
   {
-    -- "mcchrish/nnn.vim",
-    dir = "~/Projects/Neovims/nnn.vim",
+    "tamago324/lir.nvim",
     config = function()
-      require("nnn").setup({
-        -- DONE: use dedicated nnn executable with more vim friendly keymap
-        command = "nvimnnn -C -A",
-        set_default_mappings = 0,
-        replace_netrw = 1,
-        action = {
-          ["<c-p>"] = "vsplit",
-          ["<c-s>"] = "split",
-          -- ["<c-t>"] = "tab split",
-          -- ["<c-o>"] = copy_to_clipboard,
-        },
-      })
-      -- DONE: use nnn as explorer when we open directory with neovim
-      -- a vim script delete `.` buffer and close no name buffer
-      -- and then we finally get to nnn explorer window
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
+      local lir_win = require("lir.float")
+      local actions = require("lir.actions")
+      local mark_actions = require("lir.mark.actions")
+      local clipboard_actions = require("lir.clipboard.actions")
 
-      vim.cmd([[
-	augroup nnndirectory
-    autocmd!
-    autocmd StdinReadPre * let s:std_in=1
-    autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
-    \ execute 'NnnExplorer' argv()[0] | wincmd p | bd | execute 'x' | endif
-	augroup END
-      ]])
-      tmap("e", "<cmd>NnnPicker %:p:h<CR>", "[e]xplorer")
+      require("lir").setup({
+        -- NOTE: not used, add it to make my lsp happy
+        get_filters = function()
+          return {}
+        end,
+        -- NOTE: deprecated, add it to make my lsp happy
+        on_init = function() end,
+        show_hidden_files = true,
+        ignore = {
+          "node_modules",
+          ".git",
+        }, -- { ".DS_Store", "node_modules" } etc.
+        devicons = {
+          enable = true,
+          highlight_dirname = true,
+        },
+        mappings = {
+          ["<cr>"] = actions.edit,
+          ["l"] = actions.edit,
+          ["<C-s>"] = actions.split,
+          ["<C-p>"] = actions.vsplit,
+
+          ["h"] = actions.up,
+          ["q"] = actions.quit,
+
+          ["A"] = actions.mkdir,
+          ["a"] = actions.newfile,
+          ["r"] = actions.rename,
+          ["@"] = actions.cd,
+          ["y"] = actions.yank_path,
+          ["."] = actions.toggle_show_hidden,
+          ["d"] = actions.delete,
+
+          ["m"] = function()
+            mark_actions.toggle_mark("n")
+            vim.cmd("normal! j")
+          end,
+          ["Y"] = clipboard_actions.copy,
+          ["X"] = clipboard_actions.cut,
+          ["P"] = clipboard_actions.paste,
+        },
+        float = {
+          winblend = 0,
+          curdir_window = {
+            enable = false,
+            highlight_dirname = true,
+          },
+          win_opts = function()
+            local width = math.floor(vim.o.columns * 0.6)
+            local height = math.floor(vim.o.lines * 0.5)
+            return {
+              -- DONE: a telescope like border
+              border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+              width = width,
+              height = height,
+              row = math.floor((vim.o.lines - height) / 2),
+              col = math.floor((vim.o.columns - width) / 2),
+            }
+          end,
+        },
+        hide_cursor = true,
+      })
+      -- NOTE: visual mode settings
+      vim.api.nvim_create_autocmd({ "FileType" }, {
+        pattern = { "lir" },
+        callback = function()
+          -- use visual mode
+          vim.api.nvim_buf_set_keymap(
+            0,
+            "x",
+            "m",
+            ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+            { noremap = true, silent = true }
+          )
+        end,
+      })
+      -- custom folder icon
+      require("nvim-web-devicons").set_icon({
+        lir_folder_icon = {
+          icon = "",
+          color = "#7ebae4",
+          name = "LirFolderNode",
+        },
+      })
+      vim.api.nvim_create_user_command("LirToggle", function()
+        lir_win.toggle()
+      end, {})
+      vim.api.nvim_create_user_command("LirClose", function()
+        lir_win.close()
+      end, {})
+      vim.api.nvim_create_user_command("LirOpen", function()
+        lir_win.init()
+      end, {})
+      tmap("e", "<cmd>LirToggle<CR>", "[e]xplorer")
     end,
   },
 }
