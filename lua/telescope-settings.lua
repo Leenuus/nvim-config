98525054976887aa34f3afa1f49c3d72e9bf698c
Leenuus <linusrach24@gmail.com>
2024-03-13 23:24:08 +0800

    update telescope keybindings
    1. refactor leader + s keymap to smap shortcuts
    2. delete all desc, add it future maybe
    3. change file searching strategy
      - by default, search for non-hidden files in git root or fallback to
        cwd
      - a second choice `<leader>sf`, search for all files including hidden
        ones, still excluding ignore ones, in git root or fallback to cwd

--- lua/telescope-settings.lua
+++ lua/telescope-settings.lua
@@ -1,6 +1,10 @@
 local h = require("helpers")
 local nmap = h.map_normal
 
+local function smap(lhs, rhs, opts)
+  vim.keymap.set("n", "<leader>s" .. lhs, rhs, opts)
+end
+
 local actions = require("telescope.actions")
 
 require("telescope").setup({
@@ -21,31 +25,23 @@ require("telescope").setup({
 
 pcall(require("telescope").load_extension, "fzf")
 
--- Telescope live_grep in git root
--- Function to find the git root directory based on the current buffer's path
 local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
   local current_file = vim.api.nvim_buf_get_name(0)
   local current_dir
   local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
   if current_file == "" then
     current_dir = cwd
   else
-    -- Extract the directory from the current file's path
     current_dir = vim.fn.fnamemodify(current_file, ":h")
   end
 
-  -- Find the Git root directory from the current file's path
   local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   if vim.v.shell_error ~= 0 then
-    -- print("Not a git repository. Searching on current working directory")
     return cwd
   end
   return git_root
 end
 
--- Custom live_grep function to search in git root
 local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
@@ -57,40 +53,23 @@ end
 
 vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
 
-local function telescope_search_gitfiles_or_cwd()
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == "" then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ":h")
-  end
-  local _ = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    require("telescope.builtin").find_files()
-  else
-    require("telescope.builtin").git_files()
-  end
-end
+smap("l", require("telescope.builtin").resume)
+smap("h", require("telescope.builtin").help_tags)
+smap("m", require("telescope.builtin").man_pages)
+smap("s", require("telescope.builtin").builtin)
+smap("g", "<cmd>LiveGrepGitRoot<cr>")
+smap("f", function()
+  require("telescope.builtin").find_files({ hidden = true, no_ignore = true, cwd = find_git_root() })
+end)
 
-nmap("<leader>sl", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
-nmap("<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-nmap("<leader>sg", "<cmd>LiveGrepGitRoot<cr>", { desc = "[S]earch by [G]rep" })
-nmap("<leader><space>", telescope_search_gitfiles_or_cwd, { desc = "[S]earch git files or cwd" })
-nmap("<leader>sf", function()
-  require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
-end, { desc = "Search all files in current dir" })
+nmap("<leader><space>", function()
+  require("telescope.builtin").find_files({ hidden = false, no_ignore = true, cwd = find_git_root() })
+end)
 
--- nmap("<leader>sG", telescope_live_grep_open_files, { desc = "[S]earch [/] in Open Files" })
-nmap("<leader>s/", function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
+smap("/", function()
   require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
     winblend = 10,
     previewer = false,
   }))
-end, { desc = "[/] Fuzzily search in current buffer" })
+end)
 
-nmap("<leader>sm", require("telescope.builtin").man_pages, { desc = "Search Man Pages" })
