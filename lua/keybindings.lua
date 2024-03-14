5b494d958736b925fa84ab76a116e6ac0c098808
Leenuus <linusrach24@gmail.com>
2024-03-14 23:20:47 +0800

    register some whichkey key descriptions

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -95,6 +95,7 @@ require("which-key").register({
   ["<leader>d"] = { name = "[D]iagnostics", _ = "which_key_ignore" },
   ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
   ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+  ["<leader>l"] = { name = "[L]sp", _ = "which_key_ignore" },
 })
 
 -- toggle options
