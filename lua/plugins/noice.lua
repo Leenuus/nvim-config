703d44419c00ef307d9cbe1537be8e0d4da60bcc
Leenuus <linusrach24@gmail.com>
2024-03-16 19:31:11 +0800

    ui update: noice, notify
    1. new route for noice, stolen from Lazyvim
    2. notify-nvim options, may not work as expect, experimental

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -55,6 +55,30 @@ local routes = {
     kind = "wmsg",
     find = "hit BOTTOM",
   }),
+  {
+    filter = {
+      event = "msg_show",
+      any = {
+        { find = "%d+L, %d+B" },
+        { find = "; after #%d+" },
+        { find = "; before #%d+" },
+      },
+    },
+    view = "mini",
+  },
+}
+
+local opts = {
+  timeout = 3000,
+  max_height = function()
+    return math.floor(vim.o.lines * 0.75)
+  end,
+  max_width = function()
+    return math.floor(vim.o.columns * 0.3)
+  end,
+  on_open = function(win)
+    vim.api.nvim_win_set_config(win, { zindex = 100 })
+  end,
 }
 
 return {
@@ -64,6 +88,7 @@ return {
     "rcarriga/nvim-notify",
   },
   config = function()
+    require("notify").setup(opts)
     require("noice").setup({
       lsp = {
         override = {
