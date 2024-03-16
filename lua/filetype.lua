d5b9af166089162fe5fbb4b1ada82eee7d9332b7
Leenuus <linusrach24@gmail.com>
2024-03-16 19:29:57 +0800

    new filetype for newsboat rss feed

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -9,17 +9,17 @@ local pattern = {
   -- [".*/etc/foo/.*%.conf"] = { "dosini", { priority = 10 } },
   -- A pattern containing an environment variable
   -- ["${XDG_CONFIG_HOME}/foo/git"] = "git",
-  ["README.(a+)$"] = function(path, bufnr, ext)
-    if ext == "md" then
-      return "markdown"
-    elseif ext == "rst" then
-      return "rst"
-    end
-  end,
+  -- ["README.(a+)$"] = function(path, bufnr, ext)
+  --   if ext == "md" then
+  --     return "markdown"
+  --   elseif ext == "rst" then
+  --     return "rst"
+  --   end
+  -- end,
 }
 
 local filename = {
-  -- [".foorc"] = "toml",
+  ["urls"] = "rssfeed",
   -- ["/etc/foo/config"] = "toml",
 }
 
