1b6dfab82f4120269ce3b85aaf13ce5d8eb2fa31
Leenuus <linusrach24@gmail.com>
2024-03-17 23:55:58 +0800

    refactor: move recording related autocmd to dedicated module

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -72,3 +72,30 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
     end
   end,
 })
+
+
+local gp = vim.api.nvim_create_augroup("recording-notifications", { clear = true })
+
+local noption = {
+  title = "Recording",
+  timeout = 1,
+  hide_from_history = true,
+  icon = "",
+}
+local level = vim.log.levels.INFO
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  pattern = "*",
+  callback = function()
+    require("notify")("Start Recording", level, noption)
+  end,
+  group = gp,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  pattern = "*",
+  callback = function()
+    require("notify")("Stop Recording", level, noption)
+  end,
+  group = gp,
+})
