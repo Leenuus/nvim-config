cfc3b45889ddf7a0e07bbca92334615465cbb363
Leenuus <linusrach24@gmail.com>
2024-04-07 17:18:24 +0800

    fix bug: lazygit config dir
    lazygit don't work well with stow, always use bad config dir
    this fix hardcodes lazygit launch command to use config dir
    I want it to use in upstream; for detail, look at the local repo
    referenced here

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "kdheepak/lazygit.nvim",
+    -- "kdheepak/lazygit.nvim",
+    dir = "~/Projects/Neovims/lazygit.nvim",
     cmd = {
       "LazyGit",
       "LazyGitConfig",
@@ -8,7 +9,6 @@ return {
       "LazyGitFilter",
       "LazyGitFilterCurrentFile",
     },
-    -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -27,8 +27,8 @@ return {
           untracked = { text = "┆" },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
           follow_files = true,
@@ -46,7 +46,7 @@ return {
         current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil, -- Use default
+        status_formatter = nil,  -- Use default
         max_file_length = 40000, -- Disable if file is longer than this (in lines)
         preview_config = {
           -- Options passed to nvim_open_win
