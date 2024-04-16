29bb7f88c5436a9cc11b4910a91fe1efec42cf67
Leenuus <linusrach24@gmail.com>
2024-04-16 17:11:45 +0800

    add telescope select ui

--- lua/telescope-settings.lua
+++ lua/telescope-settings.lua
@@ -21,26 +21,19 @@ require("telescope").setup({
       },
     },
   },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({
+        -- even more opts
+      }),
+    },
+  },
 })
 
 pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
 
-local function find_git_root()
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  if current_file == "" then
-    current_dir = cwd
-  else
-    current_dir = vim.fn.fnamemodify(current_file, ":h")
-  end
-
-  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    return cwd
-  end
-  return git_root
-end
+local find_git_root = require('helpers').find_git_root
 
 local function live_grep_git_root()
   local git_root = find_git_root()
@@ -98,4 +91,8 @@ smap("/", function()
     winblend = 10,
     previewer = false,
   }))
+end)
+
+smap("c", function()
+  vim.cmd("Telescope neoclip")
 end)
