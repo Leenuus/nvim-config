db03e04e65ec1cd4a45778fd1066c43b4bfb65e4
Leenuus <linusrach24@gmail.com>
2024-04-16 17:11:26 +0800

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 local opts = {
-  transparent = true,     -- Enable this to disable setting the background color
+  transparent = false,     -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
 }
 
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -15,30 +15,55 @@ local header = {
   [[                                    ]],
 }
 
+local header1 = {
+  [[                                            ]],
+  [[                                            ]],
+  [[                                            ]],
+  [[             _                              ]],
+  [[            | |                             ]],
+  [[ _ __   ___ | | _____ _ __ ___   ___  _ __  ]],
+  [[| '_ \ / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ]],
+  [[| |_) | (_) |   <  __/ | | | | | (_) | | | |]],
+  [[| .__/ \___/|_|\_\___|_| |_| |_|\___/|_| |_|]],
+  [[| |                                         ]],
+  [[|_|                                         ]],
+  [[                                            ]],
+  [[                                            ]],
+}
+
 return {
   "nvimdev/dashboard-nvim",
   event = "VimEnter",
   -- enabled = false,
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
   config = function()
+    local notes_root = vim.fn.expand("$HOME/Projects/notes")
+
     require("dashboard").setup({
       theme = "doom",
       config = {
-        header = header,
+        header = header1,
         center = {
           {
             icon = "",
             desc = "New File",
             key = "n",
-            key_format = " %s", -- remove default surrounding `[]`
+            key_format = " %s",
             action = "enew",
           },
           {
             icon = "",
             desc = "Edit Config",
             key = "c",
-            key_format = " %s", -- remove default surrounding `[]`
-            action = "cd $HOME/.config/nvim|e init.lua",
+            key_format = " %s",
+            action = "cd" .. vim.fn.stdpath("config") .. "|" .. "Telescope find_files",
+          },
+          {
+            icon = "",
+            desc = "Edit Notes",
+            key = "n",
+            key_format = " %s",
+            action = "cd" .. notes_root .. "|" .. "Telescope find_files",
           },
           {
             icon = "",
