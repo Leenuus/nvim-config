dcde24bb1a8ae45329d2ff1712e685efd0df44f1
Leenuus <linusrach24@gmail.com>
2024-04-17 15:31:20 +0800

    add some todo

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -105,6 +105,7 @@ local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup({})
 
+-- TODO:  ui design, virtual text in next line
 cmp.setup({
   snippet = {
     expand = function(args)
