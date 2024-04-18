5e08bad8a3601c419beba4915f56d8b8a710ef6b
Leenuus <linusrach24@gmail.com>
2024-04-19 00:03:05 +0800

    fix bug in get_git_root

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -85,7 +85,7 @@ M.find_git_root = function()
 
   local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   if vim.v.shell_error ~= 0 then
-    return cwd
+    return current_dir
   end
   return git_root
 end
