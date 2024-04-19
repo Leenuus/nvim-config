59c606180b8f54305e9a80817e82d78493b10178
Leenuus <linusrach24@gmail.com>
2024-04-19 14:15:19 +0800

    update telescope searching
    use file dir instead of vim working dir by default
    meaning that all `vim.fn.getcwd` should be changed to
    `vim.fn.expand('%:p:h')`

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -81,7 +81,7 @@ M.augroup = function(name)
 end
 
 M.find_git_root = function()
-  local current_dir = vim.fn.getcwd()
+  local current_dir = vim.fn.expand('%:p:h')
 
   local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
   if vim.v.shell_error ~= 0 then
--- lua/tele.lua
+++ lua/tele.lua
@@ -42,77 +42,39 @@ pcall(require("telescope").load_extension, "ui-select")
 
 local find_git_root = require("helpers").find_git_root
 
-local function live_grep_file_dir()
-  local file = vim.fn.expand("%:p")
-  local dir = vim.fs.dirname(file)
-  require("telescope.builtin").live_grep({
-    cwd = dir,
-  })
-end
-
-local function live_grep_git_root()
-  local git_root = find_git_root()
-  require("telescope.builtin").live_grep({
-    cwd = git_root,
-  })
-end
-
-vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
-vim.api.nvim_create_user_command("LiveGrepFileDir", live_grep_file_dir, {})
-
 smap("l", require("telescope.builtin").resume, "Resume telescope")
 smap("h", require("telescope.builtin").help_tags, "Find Helps")
 smap("M", function()
   require("telescope.builtin").man_pages({ sections = { "ALL" } })
 end, "Find man pages")
 smap("s", require("telescope.builtin").builtin, "Show telescopes")
--- TODO: change live grep mode
-smap("g", "<cmd>LiveGrepGitRoot<cr>", "Grep Git root")
-smap("G", "<cmd>LiveGrepFileDir<cr>", "Grep current File dir")
 smap("k", "<cmd>Telescope keymaps<cr>", "Show Keymaps")
 
 local DEFAULT_FIND_FILES_MODE = "default"
 local find_files_mode = DEFAULT_FIND_FILES_MODE
 
+local base_find_command = {
+  "fd",
+  "-E",
+  "\\.git",
+  "-E",
+  "__pycache__",
+  "-t",
+  "f",
+}
+
 local find_files_options = {
   [DEFAULT_FIND_FILES_MODE] = {
     cwd = find_git_root,
-    find_command = {
-      "fd",
-      "-E",
-      "\\.git",
-      "-t",
-      "f",
-    },
+    find_command = base_find_command,
   },
-  ["git root, with hidden"] = {
+  ["hidden"] = {
     cwd = find_git_root,
-    -- NOTE: pitfall here, `vim.list_extend` change the dst list and return it
-    find_command = {
-      "fd",
-      "-E",
-      "\\.git",
-      "-t",
-      "f",
-      "-H",
-    },
+    find_command = vim.list_extend(vim.deepcopy(base_find_command), { "-H" }),
   },
-  ["cwd, all files"] = {
-    -- NOTE: pitfall
-    -- wrap it to avoid being evaluated at startup time
-    -- this should be evaluated at runtime
-    cwd = function()
-      return vim.fn.expand("%:p:h")
-    end,
-    find_command = {
-      "fd",
-      "-E",
-      "\\.git",
-      "-t",
-      "f",
-      "-H",
-      "-I",
-    },
+  ["hidden, ignore"] = {
+    cwd = find_git_root,
+    find_command = vim.list_extend(vim.deepcopy(base_find_command), { "-H", "-I" }),
   },
 }
 
@@ -129,15 +91,43 @@ smap("f", function()
   end)
 end, "Change File Searching Mode")
 
+local no_search_dirs = {
+  vim.env["HOME"],
+  "/tmp",
+}
+
 local function find_files()
   -- NOTE: pitfall, deep copy!
   local opts = vim.deepcopy(find_files_options[find_files_mode])
+  logger.info("mode: ", find_files_mode)
+  logger.info("options: ", opts)
+  -- NOTE: some dir should never be searched
+  if vim.tbl_contains(no_search_dirs, vim.fn.getcwd()) then
+    vim.cmd("cd %:p:h")
+  end
   opts["cwd"] = opts["cwd"]()
-  -- logger.info("options: ", opts)
   require("telescope.builtin").find_files(opts)
 end
 vim.api.nvim_create_user_command("FindFiles", find_files, {})
 nmap("<leader><space>", find_files)
+
+-- live grep
+local function live_grep_file_dir()
+  local dir = vim.fn.expand("%:p:h")
+  require("telescope.builtin").live_grep({
+    cwd = dir,
+  })
+end
+
+local function live_grep_git_root()
+  local git_root = find_git_root()
+  require("telescope.builtin").live_grep({
+    cwd = git_root,
+  })
+end
+
+smap("g", live_grep_git_root, "Grep Git root")
+smap("G", live_grep_file_dir, "Grep current File dir")
 
 smap("/", function()
   require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
