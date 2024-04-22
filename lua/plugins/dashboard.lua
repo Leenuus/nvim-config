fc2919b44353ea0d3bad19027b8de54a8412443e
Leenuus <linusrach24@gmail.com>
2024-04-22 14:06:04 +0800

    update dashboard
    - random startup header

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,48 +1,86 @@
-local header = {
-  [[                                    ]],
-  [[                                    ]],
-  [[                                    ]],
-  [[          ▀████▀▄▄              ▄█  ]],
-  [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█  ]],
-  [[    ▄        █          ▀▀▀▀▄  ▄▀   ]],
-  [[   ▄▀ ▀▄      ▀▄              ▀▄▀   ]],
-  [[  ▄▀    █     █▀   ▄█▀▄      ▄█     ]],
-  [[  ▀▄     ▀▄  █     ▀██▀     ██▄█    ]],
-  [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █   ]],
-  [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀   ]],
-  [[   █   █  █      ▄▄           ▄▀    ]],
-  [[                                    ]],
-  [[                                    ]],
+local headers = {
+  {
+    [[                                    ]],
+    [[                                    ]],
+    [[                                    ]],
+    [[          ▀████▀▄▄              ▄█  ]],
+    [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█  ]],
+    [[    ▄        █          ▀▀▀▀▄  ▄▀   ]],
+    [[   ▄▀ ▀▄      ▀▄              ▀▄▀   ]],
+    [[  ▄▀    █     █▀   ▄█▀▄      ▄█     ]],
+    [[  ▀▄     ▀▄  █     ▀██▀     ██▄█    ]],
+    [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █   ]],
+    [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀   ]],
+    [[   █   █  █      ▄▄           ▄▀    ]],
+    [[                                    ]],
+    [[                                    ]],
+  },
+  {
+    [[                                            ]],
+    [[                                            ]],
+    [[                                            ]],
+    [[             _                              ]],
+    [[            | |                             ]],
+    [[ _ __   ___ | | _____ _ __ ___   ___  _ __  ]],
+    [[| '_ \ / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ]],
+    [[| |_) | (_) |   <  __/ | | | | | (_) | | | |]],
+    [[| .__/ \___/|_|\_\___|_| |_| |_|\___/|_| |_|]],
+    [[| |                                         ]],
+    [[|_|                                         ]],
+    [[                                            ]],
+    [[                                            ]],
+  },
+  {
+    [[                                                       ]],
+    [[                                                       ]],
+    [[                                                       ]],
+    [[ ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
+    [[ ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
+    [[ ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
+    [[ ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║]],
+    [[ ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
+    [[ ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
+    [[                                                       ]],
+    [[                                                       ]],
+  },
+  {
+    [[                                          ]],
+    [[                                          ]],
+    [[                                          ]],
+    [[    __                                    ]],
+    [[   / /   ___  ___  ____  __  ____  _______]],
+    [[  / /   / _ \/ _ \/ __ \/ / / / / / / ___/]],
+    [[ / /___/  __/  __/ / / / /_/ / /_/ (__  ) ]],
+    [[/_____/\___/\___/_/ /_/\__,_/\__,_/____/  ]],
+    [[                                          ]],
+    [[                                          ]],
+    [[                                          ]],
+  },
 }
 
-local header1 = {
-  [[                                            ]],
-  [[                                            ]],
-  [[                                            ]],
-  [[             _                              ]],
-  [[            | |                             ]],
-  [[ _ __   ___ | | _____ _ __ ___   ___  _ __  ]],
-  [[| '_ \ / _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ ]],
-  [[| |_) | (_) |   <  __/ | | | | | (_) | | | |]],
-  [[| .__/ \___/|_|\_\___|_| |_| |_|\___/|_| |_|]],
-  [[| |                                         ]],
-  [[|_|                                         ]],
-  [[                                            ]],
-  [[                                            ]],
-}
+local notes_root = vim.env["NOTES_ROOT"]
+local notes = {}
+if notes_root ~= nil or notes_root == "" then
+  notes = {
+    icon = "󰎚  ",
+    desc = "Notes",
+    key = "n",
+    key_format = " %s",
+    action = "cd" .. notes_root .. "| FindFiles",
+  }
+end
 
 return {
-  "nvimdev/dashboard-nvim",
+  dir = "~/Projects/Neovims/dashboard-nvim",
+  -- "nvimdev/dashboard-nvim",
   event = "VimEnter",
   -- enabled = false,
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
   config = function()
-    local notes_root = vim.env['NOTES_ROOT']
-
     require("dashboard").setup({
       theme = "doom",
       config = {
-        header = header1,
+        header = headers[math.random(#headers)],
         center = {
           {
             icon = "  ",
@@ -58,13 +96,7 @@ return {
             key_format = " %s",
             action = "FindFiles",
           },
-          {
-            icon = "󰎚  ",
-            desc = "Notes",
-            key = "n",
-            key_format = " %s",
-            action = "cd" .. notes_root .. "| FindFiles",
-          },
+          notes,
           {
             icon = "  ",
             desc = "Config",
@@ -86,29 +118,6 @@ return {
             key_format = " %s",
             action = "Telescope colorscheme",
           },
-          {
-            icon = "  ",
-            desc = "Music Player",
-            key = "m",
-            key_format = " %s",
-            action = "Telescope mpc",
-          },
-          -- TODO: implement my news reader neovim frontend
-          -- {
-          --   icon = "",
-          --   desc = "News Reader",
-          --   key = "r",
-          --   key_format = " %s",
-          --   action = ":Reader",
-          -- },
-          -- TODO: Project Manager
-          -- {
-          --   icon = "",
-          --   desc = "Projects",
-          --   key = "r",
-          --   key_format = " %s",
-          --   action = ":Projects",
-          -- },
           {
             icon = "  ",
             desc = "Quit Neovim",
