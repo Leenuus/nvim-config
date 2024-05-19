f0bf7e6b0bdbee81523362e08d7eeffd712f951a
Leenuus <linusrach24@gmail.com>
2024-05-19 13:30:26 +0800

    Fix bad closing window
    It is not a good idea to use `close` for most of these filetypes, a
    better solution is `x` which won't disturb you when you try to close
    these files in last open window

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -123,8 +123,8 @@ vim.api.nvim_create_autocmd("FileType", {
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    vim.keymap.set("n", "<leader>q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "q", "<cmd>x<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "<leader>q", "<cmd>x<cr>", { buffer = event.buf, silent = true })
   end,
 })
 
