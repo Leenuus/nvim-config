9141b8ff5b8cbc91019ad2f393d6fddbf3099bfe
Leenuus <linusrach24@gmail.com>
2024-05-21 11:23:32 +0800

    ftplug

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,2 +1,7 @@
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
+
+local abbr = vim.cmd["iabbrev"]
+
+abbr("<buffer>", "true", "True")
+abbr("<buffer>", "false", "False")
---
+++ after/ftplugin/query.lua
@@ -0,0 +1,4 @@
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+
+vim.keymap.set('n', '<leader>w', '<NOP>')
