9bfaec117deb024ac2e9ec3795d59ec53c8797b7
Leenuus <linusrach24@gmail.com>
2024-05-22 16:39:35 +0800

    clean up

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,5 @@
-local opts = {
-  transparent = false,    -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-}
-
 return {
-  { "xiyaowong/transparent.nvim" },
-  {
-    "/folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.o.termguicolors = true
-      require("tokyonight").setup(opts)
-    end,
-  },
-  {
-    "Shatur/neovim-ayu",
-  },
+  "xiyaowong/transparent.nvim",
+  "/folke/tokyonight.nvim",
+  "Shatur/neovim-ayu",
 }
