202d8ee5fb7e59ef18acd6929987ca6b4d2b8f29
Leenuus <linusrach24@gmail.com>
2024-05-23 22:53:38 +0800

    lisp things

--- after/ftplugin/lisp.lua
+++ after/ftplugin/lisp.lua
@@ -1,3 +1,4 @@
-vim.bo.tabstop=2
-vim.bo.shiftwidth=2
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
 vim.bo.expandtab = true
+vim.b.minipairs_disable = true
---
+++ lua/plugins/lisp.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    "Olical/conjure",
+    ft = { "clojure", "fennel", "python", "lisp" },
+    dependencies = {
+      {
+        "PaterJason/cmp-conjure",
+        config = function()
+          local cmp = require("cmp")
+          local config = cmp.get_config()
+          table.insert(config.sources, {
+            name = "buffer",
+            option = {
+              sources = {
+                { name = "conjure" },
+              },
+            },
+          })
+          cmp.setup(config)
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+    end,
+    init = function()
+      -- Set configuration options here
+      vim.g["conjure#extract#tree_sitter#enabled"] = true
+      vim.g["conjure#client#python#stdio#command"] = "env python -iq"
+
+      vim.g["conjure#mapping#prefix"] = "<leader>"
+
+      --- mappings
+      vim.g["conjure#mapping#enable_defaults"] = false
+      vim.g["conjure#mapping#doc_word"] = "eh"
+      vim.g["conjure#mapping#log_vsplit"] = "el"
+      vim.g["conjure#log#hud#anchor"] = "SE"
+
+      -- evaluate
+      vim.g["conjure#mapping#eval_current_form"] = "ee"
+      vim.g["conjure#mapping#eval_root_form"] = "er"
+      vim.g["conjure#mapping#eval_buf"] = "ef"
+      vim.g["conjure#mapping#eval_word"] = "ew"
+      vim.g["conjure#mapping#eval_visual"] = "e"
+      vim.g["conjure#mapping#eval_comment_current_form"] = "ec"
+      vim.g["conjure#mapping#eval_marked_form"] = "em"
+      vim.g["conjure#mapping#eval_previous"] = "ep"
+      vim.g["conjure#mapping#eval_replace_form"] = "e1"
+    end,
+    enabled = true,
+  },
+  {
+    enabled = true,
+    "bhurlow/vim-parinfer",
+    ft = { "clojure", "fennel", "lisp" },
+  },
+  {
+    enabled = false,
+    "vlime/vlime",
+    ft = { "clojure", "fennel", "lisp" },
+  },
+  {
+    enabled = false,
+    "kovisoft/slimv",
+  },
+  {
+    "kovisoft/paredit",
+    enabled = false,
+  },
+}
