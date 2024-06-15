0a8bc3023b21ce8f58b80825531a92fa4a63be4e
Leenuus <linusrach24@gmail.com>
2024-06-15 19:01:19 +0800

    Template: new for python

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -2,6 +2,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
+local c = ls.choice_node
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
@@ -37,15 +38,41 @@ local home = s("home", {
   t({ "from os import environ", 'HOME = environ["HOME"]' }),
 })
 
-local disable_diagnostics = s("diag", {
-  t(" # type: ignore"),
+local disable_diagnostics = s("off", {
+  c(1, {
+    t("type: ignore"),
+    t("fmt: off"),
+  }),
 })
+
+local print = s(
+  "ic",
+  fmt(
+    [[from icecream import ic
+ic(<>)]],
+    {
+      i(0),
+    },
+    { delimiters = "<>" }
+  )
+)
+
+local ift = s(
+  "ift",
+  fmt([[<> if <> else <>]], {
+    rep(1),
+    i(1, "true"),
+    i(0, "false"),
+  }, { delimiters = "<>" })
+)
 
 local python_snips = {
   main,
   dict,
   home,
   disable_diagnostics,
+  print,
+  ift,
 }
 
 return python_snips
