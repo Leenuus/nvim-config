c3ba20d15eb7182dcf2fe1868f8ccabcfb470442
Leenuus <linusrach24@gmail.com>
2024-06-15 19:02:14 +0800

    don't search in some files when grepping

--- lua/utils/tele.lua
+++ lua/utils/tele.lua
@@ -3,25 +3,26 @@ local find_git_root = require("helpers").find_git_root
 local DEFAULT_FIND_FILES_MODE = "(gitroot)"
 local find_files_mode = DEFAULT_FIND_FILES_MODE
 
+local excluded = {
+  "*.pdf",
+  "*build*",
+  "*target*",
+  "*Pipfile*",
+  "node_modules",
+  "venv",
+  "__pycache__",
+  ".git",
+}
+
 local base_find_command = {
   "fd",
-  "-E",
-  "\\.git",
-  "-E",
-  "__pycache__",
-  "-E",
-  "venv",
-  "-E",
-  "node_modules",
-  "-E",
-  "*build*",
-  "-E",
-  "*target*",
-  "-E",
-  "*.pdf",
   "-t",
   "f",
 }
+
+for _, e in ipairs(excluded) do
+  vim.list_extend(base_find_command, { "-E", e })
+end
 
 local find_all_command = vim.list_extend(vim.deepcopy(base_find_command), { "-H", "-I" })
 
@@ -94,11 +95,18 @@ local function find_files()
   end
 end
 
+local glob_pattern = {}
+
+for _, e in ipairs(excluded) do
+  vim.list_extend(glob_pattern, { string.format("!%s", e) })
+end
+
 -- live grep
 local function grep_file_dir()
   local dir = vim.fn.expand("%:h")
   require("telescope.builtin").live_grep({
     cwd = dir,
+    glob_pattern = glob_pattern,
   })
 end
 
@@ -106,6 +114,7 @@ local function grep_git_root()
   local git_root = find_git_root()
   require("telescope.builtin").live_grep({
     cwd = git_root,
+    glob_pattern = glob_pattern,
   })
 end
 
