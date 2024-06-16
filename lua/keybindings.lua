68cc75a29be8d3eb16dce2a6b47ec6555081e8da
Leenuus <linusrach24@gmail.com>
2024-06-16 16:58:15 +0800

    Plugin File Manager: Create command and bindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -338,4 +338,4 @@ vim.keymap.set("n", "<leader><space>", "<cmd>QuickFiles<cr>")
 vim.keymap.set("n", "<leader>sf", "<cmd>FindFiles<cr>")
 vim.keymap.set("n", "<leader>sF", "<cmd>SearchMode<cr>", { desc = "Select Search Mode" })
 
-vim.keymap.set('n', '<leader>te', '<CMD>lua MiniFiles.open()<CR>', { desc = 'desc' } )
+vim.keymap.set("n", "<leader>te", "<CMD>MiniFiles<CR>", { desc = "Open File Manager" })
--- lua/plugins/utils/file-manager.lua
+++ lua/plugins/utils/file-manager.lua
@@ -14,52 +14,58 @@ return {
     {
       "echasnovski/mini.files",
       version = "*",
-      opts = {
-        content = {
-          -- Predicate for which file system entries to show
-          filter = nil,
-          -- What prefix to show to the left of file system entry
-          prefix = nil,
-          -- In which order to show file system entries
-          sort = nil,
-        },
+      config = function()
+        local opts = {
+          content = {
+            -- Predicate for which file system entries to show
+            filter = nil,
+            -- What prefix to show to the left of file system entry
+            prefix = nil,
+            -- In which order to show file system entries
+            sort = nil,
+          },
 
-        mappings = {
-          close = "<ESC>",
-          go_in = "l",
-          go_in_plus = "L",
-          go_out = "h",
-          go_out_plus = "H",
-          reset = "<BS>",
-          reveal_cwd = "@",
-          show_help = "g?",
-          synchronize = "=",
-          trim_left = "<",
-          trim_right = ">",
-        },
+          mappings = {
+            close = "<ESC>",
+            go_in = "l",
+            go_in_plus = "L",
+            go_out = "h",
+            go_out_plus = "H",
+            reset = "<BS>",
+            reveal_cwd = "@",
+            show_help = "g?",
+            synchronize = "<CR>",
+            trim_left = "<",
+            trim_right = ">",
+          },
 
-        -- General options
-        options = {
-          -- Whether to delete permanently or move into module-specific trash
-          permanent_delete = true,
-          -- Whether to use for editing directories
-          use_as_default_explorer = false,
-        },
+          -- General options
+          options = {
+            -- Whether to delete permanently or move into module-specific trash
+            permanent_delete = true,
+            -- Whether to use for editing directories
+            use_as_default_explorer = false,
+          },
 
-        -- Customization of explorer windows
-        windows = {
-          -- Maximum number of windows to show side by side
-          max_number = math.huge,
-          -- Whether to show preview of file/directory under cursor
-          preview = false,
-          -- Width of focused window
-          width_focus = 50,
-          -- Width of non-focused window
-          width_nofocus = 15,
-          -- Width of preview window
-          width_preview = 25,
-        },
-      },
+          -- Customization of explorer windows
+          windows = {
+            -- Maximum number of windows to show side by side
+            max_number = math.huge,
+            -- Whether to show preview of file/directory under cursor
+            preview = false,
+            -- Width of focused window
+            width_focus = 50,
+            -- Width of non-focused window
+            width_nofocus = 15,
+            -- Width of preview window
+            width_preview = 25,
+          },
+        }
+        require("mini.files").setup(opts)
+        vim.api.nvim_create_user_command("MiniFiles", function()
+          MiniFiles.open(vim.api.nvim_buf_get_name(0))
+        end, {})
+      end,
     },
   },
 }
