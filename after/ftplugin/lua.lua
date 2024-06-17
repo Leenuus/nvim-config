6131ddc433da03d44234778cf99e31627539e0b7
Leenuus <linusrach24@gmail.com>
2024-06-18 02:17:13 +0800

    misc

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -8,3 +8,9 @@ vim.opt_local.formatoptions:remove("o")
 -- NOTE: simple luapad alternative
 vim.keymap.set('n', '<leader>ee', '<CMD>Redir %lua<CR>', { desc = 'Vim eval current buffer' } )
 vim.keymap.set('n', '<leader>el', '<CMD>Redir .lua<CR>', { desc = 'Vim eval current buffer' } )
+
+vim.cmd[[
+ca  <buffer> pp Hey, do it in lua buffer! Use el/ee
+ca  <buffer> lua Hey, do it in lua buffer! Use el/ee, or if know what you are doing, use lau
+ca <buffer> lau lua
+]]
