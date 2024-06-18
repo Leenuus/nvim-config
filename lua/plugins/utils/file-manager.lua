775cb3781216a802bce332459a091ac065739798
Leenuus <linusrach24@gmail.com>
2024-06-19 02:58:00 +0800

    feat: better user command for minifiles

--- lua/plugins/utils/file-manager.lua
+++ lua/plugins/utils/file-manager.lua
@@ -1,70 +1,70 @@
-local function make_executable()
-  local ctx = require("lir").get_context()
-  local file = ctx:current()
-  file = file.fullpath
-  local perm = vim.fn.getfperm(file)
-  perm = perm:sub(1, 2) .. "x" .. perm:sub(4)
-  vim.fn.setfperm(file, perm)
-end
+local opts = {
+  content = {
+    -- Predicate for which file system entries to show
+    filter = nil,
+    -- What prefix to show to the left of file system entry
+    prefix = nil,
+    -- In which order to show file system entries
+    sort = nil,
+  },
 
--- TODO:
--- try open window at cursor position
+  mappings = {
+    close = "<ESC>",
+    go_in = "l",
+    go_in_plus = "L",
+    go_out = "h",
+    go_out_plus = "H",
+    reset = "<BS>",
+    reveal_cwd = "@",
+    show_help = "g?",
+    synchronize = "<leader>w",
+    trim_left = "<",
+    trim_right = ">",
+  },
+
+  -- General options
+  options = {
+    -- Whether to delete permanently or move into module-specific trash
+    permanent_delete = true,
+    -- Whether to use for editing directories
+    use_as_default_explorer = true,
+  },
+
+  -- Customization of explorer windows
+  windows = {
+    -- Maximum number of windows to show side by side
+    max_number = math.huge,
+    -- Whether to show preview of file/directory under cursor
+    preview = false,
+    -- Width of focused window
+    width_focus = 50,
+    -- Width of non-focused window
+    width_nofocus = 15,
+    -- Width of preview window
+    width_preview = 25,
+  },
+}
+
 return {
   {
     {
-      "echasnovski/mini.files",
+      -- "echasnovski/mini.files",
+      dir = "~/Projects/Neovims/mini.files",
       version = "*",
       config = function()
-        local opts = {
-          content = {
-            -- Predicate for which file system entries to show
-            filter = nil,
-            -- What prefix to show to the left of file system entry
-            prefix = nil,
-            -- In which order to show file system entries
-            sort = nil,
-          },
-
-          mappings = {
-            close = "<ESC>",
-            go_in = "l",
-            go_in_plus = "L",
-            go_out = "h",
-            go_out_plus = "H",
-            reset = "<BS>",
-            reveal_cwd = "@",
-            show_help = "g?",
-            synchronize = "<leader>w",
-            trim_left = "<",
-            trim_right = ">",
-          },
-
-          -- General options
-          options = {
-            -- Whether to delete permanently or move into module-specific trash
-            permanent_delete = true,
-            -- Whether to use for editing directories
-            use_as_default_explorer = false,
-          },
-
-          -- Customization of explorer windows
-          windows = {
-            -- Maximum number of windows to show side by side
-            max_number = math.huge,
-            -- Whether to show preview of file/directory under cursor
-            preview = false,
-            -- Width of focused window
-            width_focus = 50,
-            -- Width of non-focused window
-            width_nofocus = 15,
-            -- Width of preview window
-            width_preview = 25,
-          },
-        }
         require("mini.files").setup(opts)
-        vim.api.nvim_create_user_command("MiniFiles", function()
-          MiniFiles.open(vim.api.nvim_buf_get_name(0))
-        end, {})
+
+        vim.api.nvim_create_user_command("MiniFiles", function(args)
+          local p = args.args
+          if p == nil then
+            p = vim.api.nvim_buf_get_name(0)
+          end
+
+          local ok, _ = pcall(MiniFiles.open, p)
+          if not ok then
+            vim.notify("Not a valid path")
+          end
+        end, { nargs = "?", complete = "file" })
       end,
     },
   },
