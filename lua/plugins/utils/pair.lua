11a903ceda1a509ee51aa65bb39a5ff905f786e5
Leenuus <linusrach24@gmail.com>
2024-07-05 00:19:50 +0800

    Feat: enable pairing by default
    Also, use more aggressive pairing rules.

--- lua/plugins/utils/pair.lua
+++ lua/plugins/utils/pair.lua
@@ -1,17 +1,17 @@
 local opts = {
   modes = { insert = true, command = false, terminal = false },
   mappings = {
-    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s]" },
-    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s]" },
-    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s]" },
+    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
     [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
     ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
     ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\][%s]", register = { cr = false } },
-    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\][%s]", register = { cr = false } },
-    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\][%s]", register = { cr = false } },
+    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
   },
 }
 
@@ -29,7 +29,6 @@ return {
         end,
       })
 
-      vim.cmd("MiniPairDisable")
       require("mini.pairs").setup(opts)
     end,
   },
