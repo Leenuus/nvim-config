55793302ea932692c9d8603c9af2dbca75736189
Leenuus <linusrach24@gmail.com>
2024-07-13 23:53:17 +0800

    Feat: rofi rasi ft support

--- src/dot-config/nvim/lua/plugins/langs/lang.lua
+++ src/dot-config/nvim/lua/plugins/langs/lang.lua
@@ -1,5 +1,12 @@
 return {
-  -- renpy
-  "chaimleib/vim-renpy",
-  ft = { 'renpy' },
+  {
+    -- renpy
+    "chaimleib/vim-renpy",
+    ft = { "renpy" },
+  },
+  {
+    -- rofi rasi config file
+    "Fymyte/rasi.vim",
+    ft = { "rasi" },
+  },
 }
